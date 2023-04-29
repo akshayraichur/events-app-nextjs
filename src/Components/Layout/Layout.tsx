1ea97601aa464b9ca5db79e8e4cb8cff6b5e02c6
akshayraichur <akshay.raichur9@gmail.com>
2023-04-29 13:56:26 +0530

    feat: add header tag

--- src/Components/Layout/Layout.tsx
+++ src/Components/Layout/Layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import MainHeader from "./MainHeader";
+import Head from "next/head";
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -8,6 +9,9 @@ type LayoutProps = {
 const Layout = (props: LayoutProps) => {
   return (
     <>
+      <Head>
+        <title>Next Events App</title>
+      </Head>
       <MainHeader />
       <main>{props.children}</main>
     </>
