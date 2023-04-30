af6276588f5951bf0edcd8682bc1e0f516ca7309
akshayraichur <akshay.raichur9@gmail.com>
2023-04-30 18:34:23 +0530

    chore: add foodparty image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,6 +26,8 @@ export default function Home({ featuredEventsData }: HomeProps) {
   );
 }
 
+// Down sides of using getStaticProps
+// This page will only update if we build again. (if we do not use revalidate, if we use revalidate, then page will be built automatically after every x interval of seconds)
 export async function getStaticProps(ctx: GetStaticProps) {
   const response = await fetchData("https://react-firebase-auth-24251.firebaseio.com/events.json");
   let resultingData: Array<EventType> = Object.keys(response).map((key) => ({ id: key, ...response[key] }));
